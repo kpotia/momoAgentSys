9e97767ded60d106da7a5421ca976df845180b78
Mr Kpoti <patrickamtepe@gmail.com>
2020-12-15 16:09:14 +0000

    fix admin logout

--- admin/logout.php
+++ admin/logout.php
@@ -8,7 +8,7 @@ include_once '../db.php';
 $sql = 'INSERT INTO logs(userid,details) VALUES (:userid, :details)';
 $sth = $pdo->prepare($sql);
     $sth->bindparam(':userid', $_SESSION['id']);
-    $sth->bindparam(':empid', $details);
+    $sth->bindparam(':details', $details);
 if($sth->execute()){
 session_destroy();
 	
