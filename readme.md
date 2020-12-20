d337d351cfd3735458bc0af59e5354b86f080971
Mr Kpoti <patrickamtepe@gmail.com>
2020-12-20 14:28:29 +0000

    add readme, remv  unused files, fix header and templates

---
+++ readme.md
@@ -0,0 +1,16 @@
+# SETUP 
+
+- copy the folder in the folder 'htdocs' or 'www'
+- create a database named 'mas_db'
+- import the sql file in the folder 'SQL'
+- now run open your browser and ttype thhe url to the folder.
+
+# credentials
+
+- admin:
+	email: admin@gmail.com
+	pwd: admin@gmail.com
+
+- staff: 
+	email: staff@gmail.com
+	pwd: staff@gmail.com
--- register.php
+++ register.php
@@ -136,7 +136,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Material Design Bootstrap</title>
+  <title>Momo Agent System</title>
+  
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
   <!-- Bootstrap core CSS -->
@@ -156,39 +157,8 @@
     <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
       <div class="container-fluid">
 
-        <!-- Brand -->
-        <a class="navbar-brand waves-effect" href="#" target="_blank">
-          <strong class="blue-text">MDB</strong>
-        </a>
-
-        <!-- Collapse -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <!-- Links -->
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-          <!-- Left -->
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link waves-effect" href="#">Home
-               
-              </a>
-            </li>
-          </ul>
-
-          <!-- Right -->
-          <ul class="navbar-nav nav-flex-icons">
-            <li class="nav-item">
-              <a href="#" class="nav-link waves-effect" target="_blank">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-            </li>
-            
-          </ul>
-
+        
+        
         </div>
 
       </div>
